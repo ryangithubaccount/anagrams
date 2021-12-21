7f885941c9084ee145e0531b4679f1105183e6f1
Laura <lauraspberrypie@gmail.com>
2021-12-21 00:37:24 -0800

    added basic web app files

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+function start() {
+    console.log("game started");
+}
+
+function timer() {
+    var counter = 4;
+    var myTimer = setInterval(function() {
+      document.getElementById("timer").innerHTML = counter;
+      counter--;
+      if (counter < 0) {
+        clearInterval(myTimer);
+        document.getElementById("timer").style.color = "red";
+  
+        // do anything then time is up. 
+        window.location.href = 'endgame.html'
+      }
+    }, 1000);
+  }
+  timer();
+  
