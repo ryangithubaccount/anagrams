048e72b9dbaeaa9b5cb3e3ff11d6191021bc4a93
rchan <rchan@caltech.edu>
2021-12-23 16:51:06 -0800

    adding more flask features

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+from game_files import anagrams
 
 # initializes the Flask instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3puhljkn90W@#IT@#$MTpsdfijsfljwxczPAEsdfewi3WOE<SpaslWlksdjfj09slkdjf'
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+        num_tiles = request.form['tiles']
+        num_seconds = request.form['seconds']
+        new_game = anagrams(num_seconds, num_tiles)
     return render_template("index.html")
 
-@app.route("/game")
+
+@app.route("/game", methods=('GET', 'POST'))
 def game():
+    if request.method == 'POST':
+        # add to database 
+        return None
     return render_template("game.html")
+
+# def initialize_game():
