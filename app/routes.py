06a104071b7a1f9b9754b775e9d5cfdb64065ea1
rchan <rchan@caltech.edu>
2021-12-25 22:10:27 -0800

    Some changes to Flask and stuff. Sort of working

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, session
 from app import app
 import app.anagrams as anagrams
+
+game_instance = [None]
 
 @app.route("/", methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
         num_tiles = request.form['tiles']
         num_seconds = request.form['seconds']
-        new_game = anagrams.anagrams(num_seconds, num_tiles)
-        # return render_template("game.html")
+        game_instance[0] = anagrams.anagrams(num_seconds, num_tiles)
+        return redirect(url_for('game'))
     return render_template("index.html")
 
 
 @app.route("/game", methods=('GET', 'POST'))
 def game():
+    hand = game_instance[0].get_hand()
+    time = game_instance[0].get_time()
     if request.method == 'POST':
         # add to database 
         return None
-    return render_template("game.html")
+    return render_template("game.html", time=time, hand=hand)
